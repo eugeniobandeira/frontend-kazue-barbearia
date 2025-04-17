1a364529d2ca90b8acd190c42b2c4ba2acd56e86
Eugênio Bandeira <112191399+eugeniobandeira@users.noreply.github.com>
2025-04-17 19:58:31 -0300

    feat: update hero component with barbershop status display and routing

--- src/app/core/layout/main/components/hero/hero.component.ts
+++ src/app/core/layout/main/components/hero/hero.component.ts
@@ -1,7 +1,10 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { BarbershopService } from '@/domain/barbershop/services/barbershop.service';
+import { CommonModule } from '@angular/common';
+import { ChangeDetectionStrategy, Component, inject, OnInit } from '@angular/core';
 import { ButtonModule } from 'primeng/button';
+import { TagModule } from 'primeng/tag';
 
-const MODULES = [ButtonModule];
+const MODULES = [ButtonModule, CommonModule, TagModule];
 
 @Component({
   selector: 'app-hero',
@@ -11,4 +14,26 @@ const MODULES = [ButtonModule];
   changeDetection: ChangeDetectionStrategy.OnPush,
   standalone: true,
 })
-export class HeroComponent {}
+export class HeroComponent implements OnInit {
+  private readonly barbershopService = inject(BarbershopService);
+
+  private readonly barbershopId = 1;
+
+  barbershopInfo = this.barbershopService.barbershopInfo;
+
+  ngOnInit(): void {
+    this.loadBarbershopStatus();
+  }
+
+  private loadBarbershopStatus(): void {
+    this.barbershopService.getBarbershop(this.barbershopId).subscribe({
+      next: barbershop => {
+        this.barbershopService.barbershopStatus.set(barbershop.isOpen);
+        this.barbershopService.accessCode.set(barbershop.accessCode || '');
+      },
+      error: err => {
+        console.error('Erro ao carregar status da barbearia:', err);
+      },
+    });
+  }
+}
